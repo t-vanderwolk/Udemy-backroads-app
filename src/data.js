b09da203d30be58c952372bb3dbdc57d157771bf
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-03-13 18:24:08 -0700

    Tours and Tour components

--- src/data.js
+++ src/data.js
@@ -1,3 +1,7 @@
+import tour1 from './images/tour-1.jpeg';
+import tour2 from './images/tour-2.jpeg';
+import tour3 from './images/tour-3.jpeg';
+import tour4 from './images/tour-4.jpeg';
 export const pageLinks = [
     { id: 1, href: '#home', text: 'home' },
     { id: 2, href: '#about', text: 'about' },
@@ -32,5 +36,53 @@ export const pageLinks = [
         text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
         },
     
+  ];
+
+   export const tours = [
+    {
+    id:1,
+    image: tour1,
+    alt:'tour1',
+    date:'august 26th, 2020',
+    title:'Tibet Adventure',
+    info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+    location:'china',
+    duration:6 ,
+    cost:2100},
+
+    {
+    id:2,
+    image: tour2,
+    date:'october 1th, 2020',
+    title:'best of java',
+    info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+    location:'indonesia',
+    duration:11 ,
+    cost:1400
+    },
+
+    {
+    id:3,
+    image: tour3 ,
+    date:'september 15th, 2020',
+    title:'explore hong kong',
+    info:'Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.',
+    location:'hong kong', 
+    duration:8,
+    cost:5000
+    },
+    {
+    id:4, 
+    image: tour4 ,
+    date:'december 5th, 2019',
+    title:'kenya highlights',
+    info:'Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.', 
+    location:'kenya', 
+    duration:20,
+    cost:3300
+    }
+
+
+
   ];
   
